54b6ed8d7d7284b04a352e91aeff5cac28fdcf1d
Anass Rouimi <rouimi.anass@gmail.com>
2023-04-18 12:01:45 +0200

    Section2 : State

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import "./App.css";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Membre from "./components/Membre";
 
+const famille = {
+  membre1: {
+    nom: "Zozo",
+    age: 33,
+  },
+  membre2: {
+    nom: "Toto",
+    age: 29,
+  },
+  membre3: {
+    nom: "Floky",
+    age: 1,
+  },
+};
+
 function App(props) {
+  const [state, setState] = useState({ famille });
+
   return (
     // utilisé le Fragment pour envelopper le HTML a render
     <Fragment>
       <div className="App">
         <h1>{props.title}</h1>
         <h1>React App</h1>
-        <Membre nom="Anass" test="test" />
-        <Membre nom="Hajar" children="Je suis la Femme" />
-        <Membre nom="Floky">
+        <Membre nom={state.famille.membre1.nom} test="test" />
+        <Membre
+          nom={state.famille.membre2.nom}
+          children={state.famille.membre2.age}
+        />
+        <Membre nom={state.famille.membre3.nom}>
           <h3>Je suis un chat</h3>
         </Membre>
       </div>
